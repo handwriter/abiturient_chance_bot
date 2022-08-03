e746b8b5d3dc996153a111ca7d20e5a5a5a97678
Leonid Litvinov <leonidlitvinoff@yandex.ru>
2022-08-03 11:11:23 +0700

    Added new chance algorithm

--- main.py
+++ main.py
@@ -34,7 +34,8 @@ greet_kb = ReplyKeyboardMarkup()
 greet_kb.add(button_hi)
 
 
-def get_chance(i):
+
+def get_chance_algo_1(i):
     tds = i.find_all("td")
     op_list = tds[-1]
     value = 0
@@ -46,6 +47,20 @@ def get_chance(i):
         maxs = int(j.text.split("/")[1].split("}")[0])
         value += max(koeff * ((maxs - cur) / maxs), 0)
     return value
+
+
+def get_chance_algo_2(i):
+    tds = i.find_all("td")
+    op_list = tds[-1]
+    value = [0]
+    for j in op_list.children:
+        if j.text == "" or "{" not in j.text or "ОК [Б]" not in j.text: continue
+        if j.text.split(", ")[0] not in top_vuzes: continue
+        koeff = top_vuzes[j.text.split(", ")[0]]
+        cur = int(j.text.split("{")[1].split("/")[0])
+        maxs = int(j.text.split("/")[1].split("}")[0])
+        value.append(max(koeff * ((maxs - cur) / maxs), 0))
+    return max(value)
 
 @dp.message_handler(commands=['start'])
 async def process_start_command(message: types.Message):
@@ -91,52 +106,67 @@ async def process_start_command(message: types.Message, state: FSMContext):
             valid_3.append(i)
         if int(tds[-2].text) >= 281:
             alls.append(i)
-
+    await message.answer(f"Все: {len(alls)}"
+                         f"\nКоличество поданных согласий: {len(valid_1)}"
+                         f"\nКоличество унесших согласие в другое место: {len(valid_2)}"
+                         f"\nКоличество неопределившихся: {len(valid_3)}")
     # print("Все:", len(alls))
     # print("Количество поданных согласий:", len(valid_1))
     # print("Количество унесших согласие в другое место:", len(valid_2))
     # print("Количество неопределившихся:", len(valid_3))
-    ukhod_values = []
+
+    ukhod_values_1 = []
+    ukhod_values_2 = []
     for i in valid_2:
-        tds = i.find_all("td")
-        op_list = tds[-1]
-        value = 0
-        for j in op_list.children:
-            if j.text == "" or "{" not in j.text or "ОК [Б]" not in j.text: continue
-            if j.text.split(", ")[0] not in top_vuzes: continue
-            koeff = top_vuzes[j.text.split(", ")[0]]
-            cur = int(j.text.split("{")[1].split("/")[0])
-            maxs = int(j.text.split("/")[1].split("}")[0])
-            value += max(koeff * ((maxs - cur) / maxs), 0)
-        if value == 0: continue
-        ukhod_values.append(value)
+        value_1 = get_chance_algo_1(i)
+        if value_1 != 0: ukhod_values_1.append(value_1)
+        value_2 = get_chance_algo_2(i)
+        if value_2 != 0: ukhod_values_2.append(value_2)
 
-    # print(ukhod_values)
+    min_koef_1 = min(ukhod_values_1)
+    max_koef_1 = max(ukhod_values_1)
 
-    min_koef = min(ukhod_values)
-    max_koef = max(ukhod_values)
+    min_koef_2 = min(ukhod_values_2)
+    max_koef_2 = max(ukhod_values_2)
+
+    print(min_koef_1, max_koef_1)
+    print(min_koef_2, max_koef_2)
 
     low_chance = []
     middle_chance = []
     high_chance = []
 
     for i in valid_3:
-        chance = get_chance(i)
-        if chance < min_koef + ((max_koef - min_koef) * 0.3):
+        chance = get_chance_algo_1(i)
+        if chance < min_koef_1 + ((max_koef_1 - min_koef_1) * 0.3):
             low_chance.append(i)
-        elif chance < min_koef + ((max_koef - min_koef) * 0.6):
+        elif chance < min_koef_1 + ((max_koef_1 - min_koef_1) * 0.6):
             middle_chance.append(i)
         else:
             high_chance.append(i)
-    # print("Из них:")
-    # print("\tНизкий шанс, что уйдет:", len(low_chance))
-    # print("\tСредний шанс, что уйдет:", len(middle_chance))
-    # print("\tВысокий шанс, что уйдет:", len(high_chance))
-    await message.answer(f"Все: {len(alls)}"
-                         f"\nКоличество поданных согласий: {len(valid_1)}"
-                         f"\nКоличество унесших согласие в другое место: {len(valid_2)}"
-                         f"\nКоличество неопределившихся: {len(valid_3)}"
-                         f"\nИз них:"
+    await message.answer(f"\n---------"
+                         f"\nСтарый алгоритм"
+                         f"\nИз неопределившихся:"
+                         f"\nНизкий шанс, что уйдет: {len(low_chance)}"
+                         f"\nСредний шанс, что уйдет: {len(middle_chance)}"
+                         f"\nВысокий шанс, что уйдет: {len(high_chance)}")
+
+    low_chance.clear()
+    middle_chance.clear()
+    high_chance.clear()
+
+    for i in valid_3:
+        chance = get_chance_algo_2(i)
+        if chance < min_koef_2 + ((max_koef_2 - min_koef_2) * 0.3):
+            low_chance.append(i)
+        elif chance < min_koef_2 + ((max_koef_2 - min_koef_2) * 0.6):
+            middle_chance.append(i)
+        else:
+            high_chance.append(i)
+
+    await message.answer(f"\n---------"
+                         f"\nНовый алгоритм"
+                         f"\nИз неопределившихся:"
                          f"\nНизкий шанс, что уйдет: {len(low_chance)}"
                          f"\nСредний шанс, что уйдет: {len(middle_chance)}"
                          f"\nВысокий шанс, что уйдет: {len(high_chance)}")
